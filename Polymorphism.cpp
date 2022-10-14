49027fc3133a82bc7fcb5023f09299a7a642581d
Lee Jae-Baek <103738589+ThinkingVincent@users.noreply.github.com>
2022-10-14 14:57:51 +0900

    다형성 연습

---
+++ Polymorphism.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+
+//목적 : 
+
+class Base {
+public:
+	virtual void display() { cout << "Base" << endl; }
+};
+
+class Derived : public Base {
+public:
+	void display() { cout << "Deriver" << endl; }
+};
+
+int main() {
+	Base* ptr = new Derived;
+	ptr->display();
+}
